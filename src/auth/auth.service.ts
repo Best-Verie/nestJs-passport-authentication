bea416a44152a8dbe33ec6ab70c3e91f53f1fd9a
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-07-19 12:25:05 +0200

    validate user function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
+import { UsersService } from './../users/users.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private  usersService:UsersService){}
+
+    async validateUser(username:string,pass:string):Promise<any>{
+        const user = await this.usersService.findOne(username);
+
+        if(user && user.password === pass){
+            console.log("user alreasy exist");
+            return user;
+        }
+        return null;
+    }
+}
